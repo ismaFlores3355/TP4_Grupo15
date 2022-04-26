8ec4b08fee535faaa92aef99f3741284fa9d6eeb
ismaelFlores <isma.flores.4.66@gmail.com>
2022-04-25 22:36:42 -0300

    first commit

---
+++ src/main/java/ar/edu/unju/fi/ejercicios/Tp4Grupo15Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo15Application.class, args);
+	}
+
+}
